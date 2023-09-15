27bf04ec6b152dd6711c244f1930d05b774c9014
Paul Dommer <pdommer@coconut-capital.com>
2023-09-15 10:26:05 +0200

    Update docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,14 +14,14 @@ const config = {
   url: "https://coconut-xr.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/apfel-kruemel/",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Coconut XR", // Usually your GitHub org/user name.
   projectName: "apfel-kruemel", // Usually your repo name.
 
   trailingSlash: false,
-  
+
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
